b49caf7c40626f7a1569d62fe571b71e7bdcaba5
mallick1126 <rahulmallick1126@gmail.com>
2024-12-09 21:17:17 +0530

    change

--- src/quiz/quiz.jsx
+++ src/quiz/quiz.jsx
@@ -1,52 +1,52 @@
-import "./quiz.css";
+import "./Quiz.css";
 import { useState } from "react";
 
-const quiz() => {
-    const [currentQuestion, setCurrentQuestion] = useState(1);
+function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(1);
 
-    const handleNext = () => {
-        setCurrentQuestion((prev) => prev + 1);
-    };
+  const handleNext = () => {
+    setCurrentQuestion((prev) => prev + 1);
+  };
 
-    return (
-        <div className="quiz-container min-h-screen justify-between flex flex-col">
-            <header className="quiz-header">
-                <h1>QuizGrad</h1>
-                <span className="user-profile">Tarun ▼</span>
-            </header>
+  return (
+    <div className="quiz-container min-h-screen justify-between flex flex-col">
+      <header className="quiz-header">
+        <h1>QuizGrad</h1>
+        <span className="user-profile">Tarun ▼</span>
+      </header>
 
-            <div className="progress-bar">
-                {[1, 2, 3, 4, 5].map((step) => (
-                    <span
-                        key={step}
-                        className={`step ${currentQuestion >= step ? "active" : ""}`}
-                    ></span>
-                ))}
-            </div>
+      <div className="progress-bar">
+        {[1, 2, 3, 4, 5].map((step) => (
+          <span
+            key={step}
+            className={`step ${currentQuestion >= step ? "active" : ""}`}
+          ></span>
+        ))}
+      </div>
 
-            <main className="quiz-content mb-auto">
-                <h2>
-                    An interface design application that runs in the browser with
-                    team-based collaborative design projects
-                </h2>
-                <div className="options">
-                    <button className="option">FIGMA</button>
-                    <button className="option">ADOBE XD</button>
-                    <button className="option">INVISION</button>
-                    <button className="option">SKETCH</button>
-                </div>
-            </main>
-
-            <footer className="quiz-footer ">
-                <button className="prev">Previous</button>
-                <div className="timer">60</div>
-                <button className="next" onClick={handleNext}>
-                    Next
-                </button>
-                <button className="skip">Skip</button>
-            </footer>
+      <main className="quiz-content mb-auto">
+        <h2>
+          An interface design application that runs in the browser with
+          team-based collaborative design projects
+        </h2>
+        <div className="options">
+          <button className="option">FIGMA</button>
+          <button className="option">ADOBE XD</button>
+          <button className="option">INVISION</button>
+          <button className="option">SKETCH</button>
         </div>
-    );
+      </main>
+
+      <footer className="quiz-footer ">
+        <button className="prev">Previous</button>
+        <div className="timer">60</div>
+        <button className="next" onClick={handleNext}>
+          Next
+        </button>
+        <button className="skip">Skip</button>
+      </footer>
+    </div>
+  );
 }
 
-export default quiz;
+export default Quiz;
